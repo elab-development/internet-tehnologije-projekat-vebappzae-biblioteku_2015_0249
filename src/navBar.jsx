4745b2e89b9d7df105daa83307e561ff984f7185
comicup <miljkoviccup@gmail.com>
2025-09-15 19:17:32 +0200

    NavBar prikaz korisničkih inf i linka za prijavu

--- src/navBar.jsx
+++ src/navBar.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React from 'react';
 
+import { Link } from 'react-router-dom';
+ 
+const NavBar = ({ user }) => {
 
-function navBar() {
   return (
-    <div className="navBar">
-    <a>E-biblioteka</a>
-    </div>
+<nav className="navBar">
+<Link to="/">E-biblioteka</Link>
 
-  )
+      {user ? (
+<>
+<span>Hi, {user.first_name}</span>
+<Link to="/logout">Logout</Link>
+</>
+
+      ) : (
+<Link to="/login">Login / Register</Link>
+
+      )}
+</nav>
+
+  );
+
 }
+ 
+export default NavBar;
 
-export default navBar
+ 
