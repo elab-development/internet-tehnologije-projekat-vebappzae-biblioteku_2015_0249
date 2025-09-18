bd7bc9dae2f42513dd5cf6efe620bfb870902674
comicup <miljkoviccup@gmail.com>
2025-09-18 20:18:53 +0200

    Promena bookcontr

--- EbibliotekaV2/app/Http/Controllers/BookController.php
+++ EbibliotekaV2/app/Http/Controllers/BookController.php
@@ -15,8 +15,20 @@ class BookController extends Controller
 
     // Prikaz jedne knjige – dostupno svima
     public function show(Book $book)
-    {
-        return response()->json($book);
+{
+    $user = auth()->user();
+
+  public function show(Book $book)
+{
+    $user = auth()->user();
+
+    if(!$user || !$user->hasActiveSubscription()) {
+        // Prikaži samo prvih 10 stranica
+        $book->content = substr($book->content, 0, 10 * 1000); // npr. 1000 karaktera po stranici
     }
 
+    return response()->json($book);
 }
+
+
+
